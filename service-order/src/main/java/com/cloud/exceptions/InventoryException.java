fa57448a6e3e114d06185986afdde92701ce5475
Junior9 <moraes.junior6@gmail.com>
2025-02-25 17:52:45 -0600

    cirkut break

---
+++ service-order/src/main/java/com/cloud/exceptions/InventoryException.java
@@ -0,0 +1,8 @@
+package com.cloud.exceptions;
+
+public class InventoryException extends RuntimeException {
+
+    public InventoryException(String message){
+        super(message);
+    }
+}
--- service-order/src/main/java/com/cloud/service/InventoryService.java
+++ service-order/src/main/java/com/cloud/service/InventoryService.java
@@ -3,8 +3,11 @@ package com.cloud.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.cloud.exceptions.InventoryException;
 import com.cloud.response.ResponseApi;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,11 +17,17 @@ public class InventoryService {
     private final RestTemplate restTemplate;
     private final String URL = "http://SERVICE-INVERTORY/api/inventory/";
 
+
+    @CircuitBreaker(name = "inventory-service", fallbackMethod = "inventoryOutService")
+    @Retry(name = "inventory-service", fallbackMethod = "inventoryOutService")
     public boolean hasProductQuantity(Long productId, int quantity){
         ResponseApi respobnseApi = this.restTemplate.getForObject(URL+"has/"+productId+"/"+quantity, ResponseApi.class);
         return respobnseApi.getMessage().equals("Success") && (Boolean) respobnseApi.getData();
     }
 
+
+    @CircuitBreaker(name = "inventory-service", fallbackMethod = "inventoryOutService")
+    @Retry(name = "inventory-service", fallbackMethod = "inventoryOutService")
     public boolean decreaseInventoryProductQuantity(Long productId, int quantity){
         try {
             this.restTemplate.put(URL+"decrease/"+productId+"/"+quantity,"");
@@ -27,4 +36,9 @@ public class InventoryService {
             return false;
         }
     }
+
+    private void inventoryOutService(Exception error) {
+        throw new InventoryException("Inventory service out : " +error.getMessage());
+    }
+
 }
